3dec7f36c226a07269a364fa2316a8cf6fdb5a3b
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-01-31 22:57:21 +0300

    update

---
+++ GenericRepository/DbContext/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using GenericRepository.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenericRepository.DbContext
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.UseCollation("Turkish_CI_AS");
+        }
+
+    }
+}
